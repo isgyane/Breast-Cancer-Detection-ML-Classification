219658bdfe8ff0db28102c813cebc56ed103cddf
isgyane <isgyane92@gmail.com>
2024-08-14 22:53:11 +0000

    More structuring to the project

---
+++ scripts/_imports.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Import necessary libraries
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
+from sklearn.model_selection import cross_val_score, RandomizedSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+
